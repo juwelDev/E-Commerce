1b4366896bb75a27b496e8520a44baa8548e7a88
Dalawer Hossain Juwel <juwel@bdtask.net>
2023-10-16 11:19:55 +0600

    NextJS Install

---
+++ pages/my/non.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MyPage = () => {
+    return (
+        <div>
+            <div>
+                <h1 className="text-2xl">Hekki my non</h1>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default MyPage;
