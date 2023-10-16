cb36c7683db58311f5ac048f7ed76df22ae6b60a
Dalawer Hossain Juwel <juwel@bdtask.net>
2023-10-16 11:54:32 +0600

    Update

---
+++ pages/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div>
+            <div>
+                <h1 className="text-2xl">HomePage page</h1>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default HomePage;
