909e9c2b0a055807bb820c775bb77bb678f28ee0
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2023-10-24 13:01:04 +0600

    Dashboard Layout Create

---
+++ layouts/DashboardFooter.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import { FaFacebookF, FaInstagram, FaPhone, FaTwitter, } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { GrMail } from "react-icons/gr";
+
+
+const Footer = () => {
+  return (
+    <footer className="w-full">
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ layouts/DashboardHeader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+import { baseUrl } from "@/config/appConfig";
+import { FaBars, FaBed, FaUtensils } from "react-icons/fa";
+import { FaMattressPillow } from "react-icons/fa6";
+
+const Header = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Header;
