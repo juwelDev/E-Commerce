895cbdf9c53d65b44e2d6d77b15f315978bb9e1e
Dalawer Hossain Juwel <juwel@bdtask.net>
2023-10-25 12:43:06 +0600

    User APIDD Create

--- pages/register.tsx
+++ pages/register.tsx
@@ -7,6 +7,8 @@ import Layout from "@/layouts/Layout"
 
 
 const RegisterPage = () => {
+
+
   return (
     <Layout>
       <main className="p-5">
@@ -18,13 +20,17 @@ const RegisterPage = () => {
                 href={`${baseUrl}/login`} className="text-sm text-purple-700 hover:text-purple-600">login with existing account</Link>
           </p>
           <div className="mb-4">
-            <input placeholder="Your name" type="text" name="name" x-model="form.name" className="border-gray-300 p-2 focus:outline-none  rounded-md w-full" />
-            <small x-show="errors.name" x-text="errors.name" className="text-red-600" />
+            <input placeholder="Your First Name" type="text" name="name"  className="border-gray-300 p-2 focus:outline-none  rounded-md w-full" />
+            <small className="text-red-600"></small>
+          </div>
+          <div className="mb-4">
+            <input placeholder="Your Last Name" type="text" name="name"  className="border-gray-300 p-2 focus:outline-none  rounded-md w-full" />
+            <small className="text-red-600"></small>
           </div>
           <p />
           <div className="mb-4">
             <input placeholder="Your Email" type="email" name="email" x-model="form.email" className="border-gray-300 p-2 focus:outline-none  rounded-md w-full" />
-            <small x-show="errors.email" x-text="errors.email" className="text-red-600" />
+            <small className="text-red-600" />
           </div>
           <div className="mb-4">
             <input placeholder="Password" type="password" name="password" x-model="form.password" className="border-gray-300 focus:outline-none p-2  rounded-md w-full" />
@@ -32,7 +38,7 @@ const RegisterPage = () => {
           </div>
           <div className="mb-4">
             <input placeholder="Repeat Password" type="password" name="password" x-model="form.password_repeat" className="border-gray-300 focus:outline-none p-2  rounded-md w-full" />
-            <small x-show="errors.password_repeat" x-text="errors.password_repeat" className="text-red-600" />
+            <small className="text-red-600" />
           </div>
           <button className="btn-primary text-white bg-emerald-500 hover:bg-emerald-600 p-2 active:bg-emerald-700 w-full">
             Signup
