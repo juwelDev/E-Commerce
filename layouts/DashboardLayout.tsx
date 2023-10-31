d7466a14c46b64d9edf225f3512ecddc7ae49aa2
najmulhouda <najmulhouda6@gmail.com>
2023-10-31 21:20:20 +0600

    Products part completed

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode, useState, useEffect } from "react";
-import Head from "next/head";
-import { baseUrl } from '@/config/appConfig';
-import DashboardSidebar from "@/layouts/DashboardSidebar";
+import { baseUrl } from "@/config/appConfig";
 import DashboardHeader from "@/layouts/DashboardHeader";
+import DashboardSidebar from "@/layouts/DashboardSidebar";
+import Head from "next/head";
+import Link from "next/link";
+import { ReactNode, useEffect, useState } from "react";
 
 type Props = {
   children?: ReactNode;
@@ -10,8 +11,6 @@ type Props = {
 };
 
 const DashboardLayout = ({ children, title }: Props): JSX.Element => {
-
-
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -25,30 +24,32 @@ const DashboardLayout = ({ children, title }: Props): JSX.Element => {
     }, 500);
   }, []);
 
-
   return (
     <>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href={`${baseUrl}/img/favicon.png`} type="image/x-icon" />
+        <link
+          rel="shortcut icon"
+          href={`${baseUrl}/img/favicon.png`}
+          type="image/x-icon"
+        />
       </Head>
 
-      {
-        loading ? <div
-          className="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-white">
+      {loading ? (
+        <div className="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-white">
           <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
-        </div> :
-          <div className="flex h-screen overflow-hidden">
-            <DashboardSidebar />
-            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-              <DashboardHeader />
-              <main>
-                {children}
-              </main>
-            </div>
+        </div>
+      ) : (
+        <div className="flex h-screen overflow-hidden">
+          <DashboardSidebar />
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+            <DashboardHeader />
+            <main>{children}</main>
+            <Link href="pages/dashboard/products/products.tsx"></Link>
           </div>
-      }
+        </div>
+      )}
     </>
   );
 };
